843f6e2a36cffa7e1e2124ce0b07295a2deb2968
gxz_pc <xizhuogao@foxmail.com>
2024-04-11 22:33:58 +0800

    leetcode刷题代码

--- src/m0805.java
+++ src/m0805.java
@@ -10,11 +10,11 @@ class m0805 {
             if(A%2==0 && B%2==0){//偶偶
                 return multiply(A>>1,B>>1)<<2;
             }else if(A%2==0){//偶奇 8*5=8+8*4
-                return A+multiply(A>>1,(B-1)>>1)<<2;
+                return A+(multiply(A>>1,(B-1)>>1)<<2);//TODO 注意+优先级比<<高,需要括号
             }else if(B%2==0){//奇偶 3*4=4+2*4
-                return B+multiply((A-1)>>1,B>>1)<<2;
+                return B+(multiply((A-1)>>1,B>>1)<<2);
             }else{//奇奇 5*7=7+4*7
-                return B+multiply((A-1)>>1,B)<<1;
+                return B+(multiply((A-1)>>1,B)<<1);
             }
         }
     }
