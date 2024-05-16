add94550edcb16d696ccb2b1d51c6945f7f39a40
gxz <xizhuogao@foxmail.com>
2024-05-16 17:07:27 +0800

    希尔排序

--- src/sort/impl/ShellSort.java
+++ src/sort/impl/ShellSort.java
@@ -4,6 +4,7 @@ import sort.SortMethod;
 
 public class ShellSort implements SortMethod {
     @Override
+    //希尔排序：多组数据同时进行插入排序
     public void sort(int[] arr) {
         int n = arr.length;
         for (int gap = n / 2; gap > 0; gap /= 2) {
